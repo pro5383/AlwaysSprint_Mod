f9082049e29bae495104dd72bc32e0753eb95631
pro5383 <95685699+pro5383@users.noreply.github.com>
2025-11-06 05:24:05 +0100

    Add files via upload

---
+++ src/client/java/org/pro5383/always_sprint/client/Always_sprintClient.java
@@ -0,0 +1,23 @@
+package org.pro5383.alwayssprint.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+
+@Environment(EnvType.CLIENT)
+public class AlwaysSprintClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(this::onTick);
+    }
+
+    private void onTick(MinecraftClient client) {
+        if (client.player != null && client.currentScreen == null) {
+            KeyBinding.setKeyPressed(client.options.sprintKey.getDefaultKey(), true);
+        }
+    }
+}
---
+++ src/main/java/org/pro5383/always_sprint/Always_sprint.java
@@ -0,0 +1,10 @@
+package org.pro5383.always_sprint;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Always_sprint implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
